6dfd601d0c0aab004af47b9c899bf8b771d149c8
go2023 <tetyanago2018@gmail.com>
2024-08-19 16:31:11 -0700

    Routes and Route were implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import {Navigate, Route, Routes} from "react-router-dom";
+import {Container} from "react-bootstrap";
 
 function App() {
 
-  return <h1>Hi!</h1>
+  return (
+      <Container>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/new" element={<h1>New</h1>} />
+          <Route path="/:id">
+            <Route index element={<h1>Show</h1>}/>
+            <Route path={"edit"} element={<h1>Edit</h1>}/>
+          </Route>
+          <Route path="*" element={<Navigate to={"/"}/>} />
+        </Routes>
+      </Container>
+ )
 }
 
 export default App
