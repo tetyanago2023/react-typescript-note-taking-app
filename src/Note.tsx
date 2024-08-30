a2f2f456a329681f7afaefe543053db7ae70fbba
go2023 <tetyanago2018@gmail.com>
2024-08-30 13:58:54 -0700

    react-markdown stuff was implemented

--- src/Note.tsx
+++ src/Note.tsx
@@ -1,35 +1,52 @@
-import {useNote} from "./NoteLayout.tsx";
-import {Badge, Button, Col, Row, Stack} from "react-bootstrap";
-import {Link} from "react-router-dom";
+import { Badge, Button, Col, Row, Stack } from "react-bootstrap"
+import { Link, useNavigate } from "react-router-dom"
+import { useNote } from "./NoteLayout"
+import ReactMarkdown from "react-markdown"
 
-export function Note() {
+type NoteProps = {
+    onDelete: (id: string) => void
+}
+
+export function Note({ onDelete }: NoteProps) {
     const note = useNote()
+    const navigate = useNavigate()
 
-    return <>
-        <Row className="align-items-center mb-4">
-            <Col>
-                <h1>{note.title}</h1>
-                {note.tags.length > 0 && (
-                    <Stack gap={1} direction="horizontal" className="flex-wrap">
-                        {note.tags.map(tag => (
-                            <Badge className="text-truncate" key={tag.id}>
-                                {tag.label}
-                            </Badge>
-                        ))}
+    return (
+        <>
+            <Row className="align-items-center mb-4">
+                <Col>
+                    <h1>{note.title}</h1>
+                    {note.tags.length > 0 && (
+                        <Stack gap={1} direction="horizontal" className="flex-wrap">
+                            {note.tags.map(tag => (
+                                <Badge className="text-truncate" key={tag.id}>
+                                    {tag.label}
+                                </Badge>
+                            ))}
+                        </Stack>
+                    )}
+                </Col>
+                <Col xs="auto">
+                    <Stack gap={2} direction="horizontal">
+                        <Link to={`/${note.id}/edit`}>
+                            <Button variant="primary">Edit</Button>
+                        </Link>
+                        <Button
+                            onClick={() => {
+                                onDelete(note.id)
+                                navigate("/")
+                            }}
+                            variant="outline-danger"
+                        >
+                            Delete
+                        </Button>
+                        <Link to="/">
+                            <Button variant="outline-secondary">Back</Button>
+                        </Link>
                     </Stack>
-                )}
-            </Col>
-            <Col xs="auto">
-                <Stack direction="horizontal" gap={2}>
-                    <Link to={`/${note.id}/edit`}>
-                        <Button variant="primary">Edit</Button>
-                    </Link>
-                    <Button variant="outline-danger">Delete</Button>
-                    <Link to={"/"}>
-                        <Button variant="outline-secondary">Back</Button>
-                    </Link>
-                </Stack>
-            </Col>
-        </Row>
-    </>
+                </Col>
+            </Row>
+            <ReactMarkdown>{note.markdown}</ReactMarkdown>
+        </>
+    )
 }
